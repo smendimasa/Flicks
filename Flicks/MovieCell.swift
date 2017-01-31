e26bd8ccb6364011465fec6e9c9654694ff5ca6a
Sam Mendimasa <z@Mac-User-2.local>
2017-01-31 10:36:00 -0500

    additional files added

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Sam Mendimasa on 1/29/17.
+//  Copyright © 2017 Sam Mendimasa. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
